69dff848094d7a0548855b403e625b8753921c7e
Geronimo Velasco <geronimo.velasco@gsdplus.com>
2022-07-16 20:46:02 -0500

    human reaction test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-game';
+  startButtonText = 'COMENZAR';
+  hasGameStarted = false;
+  hasGameFinished = false;
+  reactionTime = 0;
+
+  startGame() {
+    this.reactionTime = 0;
+    this.hasGameFinished = false;
+    this.startButtonText = 'PREPARATE';
+    setTimeout(() => {
+      this.hasGameStarted = true;
+      this.reactionTime = new Date().getTime();
+    }, Math.floor(Math.random() * 11) * 1000);
+  }            
+
+  finishGame() {
+    this.reactionTime = new Date().getTime() - this.reactionTime;
+    this.startButtonText = 'COMENZAR';
+    this.hasGameFinished = true;
+    this.hasGameStarted = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
+  //only for components
   declarations: [
     AppComponent
   ],
+  //Only for modules
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
+  // First component
   bootstrap: [AppComponent]
 })
 export class AppModule { }
